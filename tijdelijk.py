59c6d7d329db5f4320aa9d5d032ee2ed827768b3
ThomasdenOtter <140242757+ThomasdenOtter@users.noreply.github.com>
2024-01-25 15:43:06 +0100

    bestand tijdelijk.py gecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -8,6 +8,13 @@ aanbieding = prijzen['vanille'] * 0.8
 
 reclame_tekst= f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
 
-print(reclame_tekst)
+reclametekst2= reclame_tekst[:62]
+reclametekst3= reclametekst2.upper()
+reclametekst4= [el.lower() if len(el) <=4 else el.upper() for el in list(reclametekst3)]
+
+
+for el in reclametekst4:
+     print(el)
+
 
 
