32764edcb04467002d605ffcd9e2555e3129fd25
ThomasdenOtter <140242757+ThomasdenOtter@users.noreply.github.com>
2024-01-25 14:56:20 +0100

    huiswerk 7_1

---
+++ tijdelijk.py
@@ -0,0 +1,13 @@
+prijzen = {
+    "aardbei" : int(3),
+    "vanille" : int(4),
+    "chocolade": int(5)
+    }
+
+aanbieding = prijzen['vanille'] * 0.8
+
+reclame_tekst= f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+
+print(reclame_tekst)
+
+
